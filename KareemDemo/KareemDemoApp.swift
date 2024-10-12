27495971c381ef3ec7fd9f053307b5a1c2cfa60f
KareemAbdElSattar <kareem.abdelsattar@vezeeta.com>
2024-10-12 14:05:03 +0300

    Initial Commit

---
+++ KareemDemo/KareemDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KareemDemoApp.swift
+//  KareemDemo
+//
+//  Created by KareemAbdElSattar on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct KareemDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KareemDemoTests/KareemDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  KareemDemoTests.swift
+//  KareemDemoTests
+//
+//  Created by KareemAbdElSattar on 12/10/2024.
+//
+
+import Testing
+@testable import KareemDemo
+
+struct KareemDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
